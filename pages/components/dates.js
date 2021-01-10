0dfe3e8c21a492a6db76db7a605657a411206bcf
LanguageXange <hlin0017@seattlecentral.edu>
2021-01-10 14:43:39 -0800

    adding key

--- pages/components/dates.js
+++ pages/components/dates.js
@@ -3,5 +3,5 @@ import { parseISO, format } from "date-fns";
 
 export default function Date({ dateString }) {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, "yyyy, LLLL dd")}</time>;
+  return <time dateTime={dateString}>{format(date, "LLLL dd yyyy")}</time>;
 }
