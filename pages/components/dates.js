f6aa5632e5db3e38af3f30a131227ecabe7e7850
LanguageXange <hlin0017@seattlecentral.edu>
2021-01-10 17:00:55 -0800

    comment out date file temporary invalid error from verce build

--- pages/components/dates.js
+++ pages/components/dates.js
@@ -1,8 +1,8 @@
-import { parseISO, format } from "date-fns";
+//import { parseISO, format } from "date-fns";
 //parseISO now validates separate date and time values in ISO-8601 strings and returns Invalid Date if the date is invalid.
 
-export default function Date({ dateString }) {
-  const date = parseISO(dateString);
+// export default function Date({ dateString }) {
+//   const date = parseISO(dateString);
 
-  return <time dateTime={dateString}>{format(date, "LLLL d yyyy")}</time>;
-}
+//   return <time dateTime={dateString}>{format(date, "LLLL d yyyy")}</time>;
+// }
